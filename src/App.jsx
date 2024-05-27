a5153ceaa1e1e008bd83502e608cdc639136f9ad
Grant-Howard509 <howardgrant120@gmail.com>
2024-05-27 15:17:24 -0700

    GraphQL Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { createStorefrontApiClient } from '@shopify/storefront-api-client';
 
+const client = createStorefrontApiClient({
+  storeDomain: 'https://ricebandmerch.myshopify.com/',
+  apiVersion: '2024-04',
+  publicAccessToken: 'PUBLIC_KEY_HERE',
+});
+
+
+function App() {
+
+const [data, setData] = useState(null);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+
+
+const productQuery = `
+  {
+    product(id: "gid://shopify/Product/9563122499893") {
+      title
+      id
+      handle
+      description
+      createdAt
+      totalInventory
+      images(first: 1) {
+        nodes {
+          src
+        }
+      }
+    }
+  }
+  `;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await client.request(productQuery);
+        setData(response.data.product);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if(error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  let date = new Date(data.createdAt);
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>{data.title}</h1>
+     <p>{data.description}</p>
+     <p>{date.toDateString()}</p>
+     <p>{data.totalInventory}</p>
+     <img src={data.images.nodes[0].src} width={500}/>
     </>
   )
 }
