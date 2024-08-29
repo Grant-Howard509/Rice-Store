b3ed381dba990e44a60cf2efd892483025b6e20c
Grant-Howard509 <howardgrant120@gmail.com>
2024-08-29 14:48:45 -0700

    react-intersection-observer

--- src/App.jsx
+++ src/App.jsx
@@ -4,64 +4,81 @@ import  { NavBar } from './Components/NavBar'
 import { Home }  from './Components/Home'
 import { Shop } from './Components/Shop'
 import { Route, Routes, useLocation} from 'react-router-dom'
+import { useInView} from 'react-intersection-observer';
 
 
 export default function App() {
   const [color, setColor] = useState('text-white');
   const homeRef = useRef();
-  const shopRef = useRef();
-  const releaseRef = useRef();
-  const landingPageRef = useRef();
   const location = useLocation();
-  let refList = [homeRef, shopRef, landingPageRef,releaseRef];
-  
+
+  const [scrollDirection, setScrollDirection] = useState('down');
+  const [prevYPosition, setPrevYPosition] = useState(0);
+  const [activeSection, setActiveSection] = useState('landingPage');
+
   useEffect(() => {
-    const options = {
-      root: null,
-      threshold: [.97],
-    };
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
 
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        // Multiple entries possible solution:
-        // Use id's to detect section navBar color
-        if(entry.isIntersecting) {
-          setColor('text-black');
-        }else {
-          setColor('text-white');
-;        }
-      });
-    }, options);
-
-    const initObserver = () => {
-      for (let ref of refList) {
-        if (ref.current) {
-          observer.observe(ref.current);
-        }
+      if (currentScrollY > prevYPosition) {
+        setScrollDirection('down');
+      } else {
+        setScrollDirection('up');
       }
-    };
 
-    const timer = setTimeout(initObserver, 10);
+      setPrevYPosition(currentScrollY);
+    }
+
+    window.addEventListener('scroll', handleScroll);
 
     return () => {
-      clearTimeout(timer);
-      
-      for (let ref of refList) {
-        if (ref.current) {
-          observer.unobserve(ref.current);
-        }
-      }
+      window.removeEventListener('scroll', handleScroll);
     }
-  }, [location.pathname]);
+  }, [prevYPosition]);
+
+
+
+  const {ref: releaseObserverRef, inView: isReleaseView} = useInView({
+    threshold: scrollDirection === 'down' && activeSection !== 'release'? 0.98 : 0,
+    onChange: (inView) => {
+      if (inView) setActiveSection('release');
+    }
+  });
+
+  const {ref: landingObserverPageRef, inView: isLandingPageInView} = useInView({
+    threshold: scrollDirection === 'down' && activeSection !== 'landingPage' ? 0.98 : 0,
+    onChange: (inView) => {
+      if (inView) setActiveSection('landingPage');
+    }
+  });
+
+  const {ref: shopObserverRef, inView: isShopInView} = useInView({
+    threshold: scrollDirection === 'down' && activeSection !== 'shop' ? 0.98 : 0,
+    onChange: (inView) => {
+      if (inView) setActiveSection('shop');
+    }
+  });
+
+  useEffect(() => {
+    if (activeSection === 'release') {
+      setColor('text-black');
+    } else if (activeSection === 'landingPage') {
+      setColor('text-white');
+    } else if (activeSection === 'shop') {
+      setColor('text-green-500');
+    }
+  }, [activeSection, location.pathname, scrollDirection]);
 
   return (
     <div className='relative scroll-smooth' id='wrapper'>
       <NavBar color={color}/>
 
       <Routes>
-        <Route path='/' element={<Home ref={homeRef} releaseRef={releaseRef} landingPageRef={landingPageRef} key='home'/>} />
-        <Route path='/shop' element={<Shop ref={shopRef} key='shop'/>} />
+        <Route path='/' element={<Home ref={homeRef} releaseRef={releaseObserverRef} landingPageRef={landingObserverPageRef} key='home'/>} />
+        <Route path='/shop' element={<Shop ref={shopObserverRef} key='shop'/>} />
       </Routes>
+
+      <section className='w-full h-screen'>he</section>
     </div>
   )
 }
