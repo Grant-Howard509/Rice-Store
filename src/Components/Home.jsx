985e7fd354ba97cbc8409a6329bff5e8520fd231
Grant-Howard509 <howardgrant120@gmail.com>
2024-08-30 08:20:41 -0700

    TourSection start

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import { forwardRef } from "react";
 import { LandingPage } from "./LandingPage";
 import { ReleaseSection } from "./ReleaseSection";
+import { TourSection } from "./TourSection";
 
 
 export const Home = forwardRef(({releaseRef, landingPageRef}, ref) => {
@@ -8,6 +9,7 @@ export const Home = forwardRef(({releaseRef, landingPageRef}, ref) => {
         <div ref={ref}>
             <LandingPage ref={landingPageRef}/>
             <ReleaseSection ref={releaseRef}/>
+            <TourSection />
         </div>
     );
 });
