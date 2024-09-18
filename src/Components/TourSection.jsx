55b9bbaf9c7e1899ad773205d5ba3ea517ce4775
Grant-Howard509 <howardgrant120@gmail.com>
2024-09-18 11:38:49 -0700

    Release section refactor

--- src/Components/TourSection.jsx
+++ src/Components/TourSection.jsx
@@ -1,14 +1,6 @@
 
 export const TourSection = () => {
     return (
-        <div className="flex flex-col justify-center items-center w-full h-screen sm:p-20 p-11">
-            <div className="self-start">
-                <h1 className="font-bebas xl:text-9xl md:text-7xl text-5xl">Tour</h1>
-            </div>
-
-            <div className="bg-[url('/images/Album.jpeg')] md:w-full w-full aspect-[1/1] bg-contain bg-left bg-no-repeat cursor-pointer">
-                
-            </div>
-        </div>
+        <></>
     );
 };
