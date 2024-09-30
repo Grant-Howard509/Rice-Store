e2c04971dfeb7f406a5ad9a1c153b09bd7ef45ec
Grant-Howard509 <howardgrant120@gmail.com>
2024-09-30 16:22:51 -0700

    White border divider

--- src/Components/TourSection.jsx
+++ src/Components/TourSection.jsx
@@ -2,53 +2,60 @@
 export const TourSection = () => {
     return (
         <div className="w-full h-screen bg-red-600 flex justify-center bg-[url('/images/Tour-Background.jpeg')] bg-cover bg-center bg-fixed">
-            <div className="w-full max-w-7xl flex justify-center items-center flex-col text-white">
-                <h1 className="text-9xl mb-8">Tour Section</h1>
+            <div className="w-full max-w-7xl flex justify-center items-center flex-col text-white font-bebas ">
+                <h1 className="text-9xl mb-10">Tour Section</h1>
                 <div className="flex w-full justify-evenly text-xl bg-slate-700 flex-col">
 
-                    <div className="flex w-full justify-evenly text-xl bg-slate-700 p-5">
-                        <div>
-                            <div>Oct 5th, 2024</div>
-                            <div>Ziggo Dome</div>
-                            <div>Sold Out</div>
+                    <div className="border-b border-white">
+                        <div className="flex w-full justify-around text-xl bg-slate-800 p-5">
+                            <div>
+                                <div>Oct 5th, 2024</div>
+                                <div>Ziggo Dome</div>
+                                <div>Sold Out</div>
+                            </div>
+
+                            <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
+
+                            <div className="flex justify-center items-center">
+                                <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                            </div>
                         </div>
+                    </div>
 
-                        <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
+                    <div className="border-b border-white">
+                        <div className="flex w-full justify-around text-xl bg-slate-800 p-5">
+                            <div>
+                                <div>Oct 5th, 2024</div>
+                                <div>Ziggo Dome</div>
+                                <div>Sold Out</div>
+                            </div>
 
-                        <div className="flex justify-center items-center">
-                            <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                            <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
+
+                            <div className="flex justify-center items-center">
+                                <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                            </div>
                         </div>
                     </div>
 
 
-                    <div className="flex w-full justify-evenly text-xl bg-slate-700 p-5">
-                        <div>
-                            <div>Oct 5th, 2024</div>
-                            <div>Ziggo Dome</div>
-                            <div>Sold Out</div>
-                        </div>
+                    <div className="border-b border-white">
+                        <div className="flex w-full justify-around text-xl bg-slate-800 p-5">
+                            <div>
+                                <div>Oct 5th, 2024</div>
+                                <div>Ziggo Dome</div>
+                                <div>Sold Out</div>
+                            </div>
 
-                        <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
+                            <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
 
-                        <div className="flex justify-center items-center">
-                            <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
-                        </div>
-                    </div>
-
-
-                    <div className="flex w-full justify-evenly text-xl bg-slate-700 p-5">
-                        <div>
-                            <div>Oct 5th, 2024</div>
-                            <div>Ziggo Dome</div>
-                            <div>Sold Out</div>
-                        </div>
-
-                        <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
-
-                        <div className="flex justify-center items-center">
-                            <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                            <div className="flex justify-center items-center">
+                                <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                            </div>
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
