85f9f91a3ad632993f92ea542717f8f7395c4206
Grant-Howard509 <howardgrant120@gmail.com>
2024-09-30 16:05:42 -0700

    Test layout
    First draft of tour section layout. Planning on pulling data from a google sheet so band members can insert, update, and delete tour entries.

--- src/Components/TourSection.jsx
+++ src/Components/TourSection.jsx
@@ -1,49 +1,54 @@
 
 export const TourSection = () => {
     return (
-        <div className="bg-[url('/images/Tour-Background.jpeg')] w-full h-screen bg-cover bg-no-repeat flex justify-center items-center flex-col text-white">
-            <div className="flex flex-col gap-10 w-3/5">
-                <h1 className="text-9xl font-bebas">Tour Dates</h1>
-                <div className="text-2xl w-full bg-slate-700 p-10 rounded">
-                    {/* <table className="table-fixed w-full">
-                        <tr>
-                            <th className="w-1/4">Date</th>
-                            <th>Venue</th>
-                            <th>State</th>
-                            <th>Tickets</th>
-                        </tr>
-                        <tr>
-                            <td>10/5/2024</td>
-                            <td>Hops Festival Yakima</td>
-                            <td>Wahington</td>
-                            <td>Tickets</td>
-                        </tr>
-                    </table> */}
+        <div className="w-full h-screen bg-red-600 flex justify-center bg-[url('/images/Tour-Background.jpeg')] bg-cover bg-center bg-fixed">
+            <div className="w-full max-w-7xl flex justify-center items-center flex-col text-white">
+                <h1 className="text-9xl mb-8">Tour Section</h1>
+                <div className="flex w-full justify-evenly text-xl bg-slate-700 flex-col">
 
-                    <table className="table-fixed">
-                        <thead>
-                            <tr className="text-4xl">
-                                <th className="w-1/3 text-left py-4">Date</th>
-                                <th className="w-1/3 text-left py-4">Venue</th>
-                                <th className="w-1/4 text-left y-4">State</th>
-                                <th className="w-1/3 text-left y-4">Tickets</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td className="text-left py-2">09/21/2024</td>
-                                <td className="text-left py-2">Blue Moon Tavern</td>
-                                <td className="text-left py-2">Washington</td>
-                                <td className="text-left py-2">$10 Cash</td>
-                            </tr>
-                            <tr>
-                                <td className="text-left py-2">10/5/2024</td>
-                                <td className="text-left py-2">Hops Festival Yakima</td>
-                                <td className="text-left py-2">Washington</td>
-                                <td className="text-left py-2 text-blue-300"><a href="https://freshhopalefestival.com/">Website</a></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div className="flex w-full justify-evenly text-xl bg-slate-700 p-5">
+                        <div>
+                            <div>Oct 5th, 2024</div>
+                            <div>Ziggo Dome</div>
+                            <div>Sold Out</div>
+                        </div>
+
+                        <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
+
+                        <div className="flex justify-center items-center">
+                            <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                        </div>
+                    </div>
+
+
+                    <div className="flex w-full justify-evenly text-xl bg-slate-700 p-5">
+                        <div>
+                            <div>Oct 5th, 2024</div>
+                            <div>Ziggo Dome</div>
+                            <div>Sold Out</div>
+                        </div>
+
+                        <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
+
+                        <div className="flex justify-center items-center">
+                            <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                        </div>
+                    </div>
+
+
+                    <div className="flex w-full justify-evenly text-xl bg-slate-700 p-5">
+                        <div>
+                            <div>Oct 5th, 2024</div>
+                            <div>Ziggo Dome</div>
+                            <div>Sold Out</div>
+                        </div>
+
+                        <div className="flex justify-center items-center"> Amsterdam, Netherlands</div>
+
+                        <div className="flex justify-center items-center">
+                            <button className="bg-green-600 pl-2 pr-2 rounded">Tickets</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
